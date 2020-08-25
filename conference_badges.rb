679ed11cae4b76a32a0d4b4934a026e7a848f968
April Adams <illusionlace@gmail.com>
2020-08-25 08:07:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 def badge_maker(speaker)
   return "Hello, my name is #{speaker}."
 end
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers)
 speakers.collect{|speaker| badge_maker(speaker)}
 end
