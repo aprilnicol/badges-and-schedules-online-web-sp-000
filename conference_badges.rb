9f4f5e6e18270fa285bf5490ebbbef5c234d64b1
April Adams <illusionlace@gmail.com>
2020-08-25 07:39:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,6 @@ return messages
 end
 def assign_rooms(speakers)
   greeting = []
-  speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
   speakers.each_with_index{|speakers, index| greeting << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
   return greeting
 end
