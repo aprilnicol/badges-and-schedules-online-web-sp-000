de6f1df8e34421febacee727b583799bd5819b65
April Adams <illusionlace@gmail.com>
2020-08-25 07:48:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,15 @@ names.each do |name|
 end
 return messages
 end
+def assign_rooms(rooms)
+  rooms.each do |badge| 
+    puts badge
+  end 
+  def printer(speaker)
+    batch_badge_creator(speaker).each do |badge| 
+      put badge 
+    end 
+    assign_rooms(speakers).each do |room|
+      puts room 
+    end 
+  end 
